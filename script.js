179176b77ad54d64fe99b99d5f39d47770da48a6
shizuini <0231149015@shisu.edu.cn>
2026-07-27 08:30:06 +0800

    Add task management functions in script.js

--- script.js
+++ script.js
@@ -1,1 +1,47 @@
+let task = "";
 
+function startFocus(){
+
+    task = document.getElementById("taskInput").value;
+
+    if(task === ""){
+        task = "完成一个专注任务";
+    }
+
+
+    document.getElementById("showTask").innerHTML = task;
+
+
+    changePage("home","focus");
+
+
+}
+
+
+
+function finishFocus(){
+
+    changePage("focus","feedback");
+
+}
+
+
+
+function getReward(){
+
+    changePage("feedback","reward");
+
+}
+
+
+
+function changePage(oldPage,newPage){
+
+    document.getElementById(oldPage)
+    .classList.remove("active");
+
+
+    document.getElementById(newPage)
+    .classList.add("active");
+
+}
